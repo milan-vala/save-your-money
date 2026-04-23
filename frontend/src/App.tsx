dcf524b589e5f93ca561bfe22e046806c50facbf
Milan Vala <milanvala31@gmail.com>
2026-04-23 19:37:49 +0530

    test husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { RootLayout } from "./layouts/RootLayout.tsx";
-import { Home } from "./pages/home.tsx";
-import { Login } from "./pages/login.tsx";
+import { Home } from "@src/pages/home.tsx";
+import { Login } from "@src/pages/login.tsx";
 
 function App() {
   return (
